bae9e1849175b9a91c6256e952faaed33efb881c
Kazmi Yamazato <kazmi.yamazato@gmail.com>
2021-10-10 13:16:53 +0900

    Initial Commit

---
+++ Library App/LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryApp.swift
+//  Library App
+//
+//  Created by 山里カズミ on 2021/10/10.
+//
+
+import SwiftUI
+
+@main
+struct LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
